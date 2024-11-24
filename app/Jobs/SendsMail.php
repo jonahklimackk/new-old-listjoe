0ed58882d8ff9e44eb4ec9ef652e3ba68b93c440
jonahklimackk <jonahklimackk@gmail.com>
2024-11-24 16:29:12 -0500

    x

--- app/Jobs/SendsMail.php
+++ app/Jobs/SendsMail.php
@@ -41,6 +41,7 @@ class SendsMail implements ShouldQueue
     public function handle()
     {
         $recipients = User::get()->random($this->mailing->recipients)->all();
+        dump($recipients);
 
         foreach ($recipients as $recipient)
         {
