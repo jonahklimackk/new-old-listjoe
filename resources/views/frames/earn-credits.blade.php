6c66153f23c8b2df9cebc11081eda052b5aceb35
jonahklimackk <jonahklimackk@gmail.com>
2024-11-25 14:47:40 -0500

    fixed top frame sourcing wrong datbase

--- resources/views/frames/earn-credits.blade.php
+++ resources/views/frames/earn-credits.blade.php
@@ -2,7 +2,7 @@
 
 <html>
  <frameset rows="50,*"> 
-	<frame src="http://localhost:8000/frames/earn-credits-top-frame/{{ $creditClick->key }}">
+	<frame src="/frames/earn-credits-top-frame/{{ $creditClick->key }}">
 	<frame src="http://listjoe.com"> 
 </frameset>
  </html>
