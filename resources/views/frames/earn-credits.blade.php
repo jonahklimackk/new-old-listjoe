a8cdb4e1f970f659f32d804b25ffd18f0e281efa
jonahklimackk <jonahklimackk@gmail.com>
2024-11-25 11:42:26 -0500

    go

--- resources/views/frames/earn-credits.blade.php
+++ resources/views/frames/earn-credits.blade.php
@@ -1,10 +1,5 @@
-d<!DOCTYPE html> 
+<!DOCTYPE html> 
 
-<?php
-
-dump("hi");
-dd($creditClick);
-?>
 <html>
  <frameset rows="50,*"> 
 	<frame src="http://localhost:8000/frames/earn-credits-top-frame/{{ $creditClick->key }}">
