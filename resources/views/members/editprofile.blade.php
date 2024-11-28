638b3e7cddd2cd4e0fb53a21692a3dd511f5e2fd
jonahklimackk <jonahklimackk@gmail.com>
2024-11-28 10:15:20 -0500

    put

--- resources/views/members/editprofile.blade.php
+++ resources/views/members/editprofile.blade.php
@@ -103,7 +103,7 @@
                  @endif
               </a>                    </div>
 
- <form action="/posts/store" enctype="multipart/form-data" method="post">
+ <form action="/posts/store" enctype="multipart/form-data" method="put">
                 @csrf
 <!--                 <div class="mb-3">
                     <label>Title</label>
