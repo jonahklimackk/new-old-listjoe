987bd96b5d1629b6110f7ce8d836e5b5f1509eac
jonahklimackk <jonahklimackk@gmail.com>
2024-12-03 13:54:00 -0500

    f

--- config/listjoe.php
+++ config/listjoe.php
@@ -78,8 +78,8 @@ return [
     |
     */
 
-    // 'email_url' => 'http://104.248.123.185',
-    'email_url' => 'http://localhost:8000',
+    'email_url' => 'http://104.248.123.185',
+    // 'email_url' => 'http://localhost:8000',
 
 
 
