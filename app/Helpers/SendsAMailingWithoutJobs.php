84c3e7bcb6ac5365b84dfd0bd90eb6b62ff275d3
jonahklimackk <jonahklimackk@gmail.com>
2024-12-17 13:40:36 -0500

    mailer

--- app/Helpers/SendsAMailingWithoutJobs.php
+++ app/Helpers/SendsAMailingWithoutJobs.php
@@ -77,7 +77,7 @@ class SendsAMailingWithoutJobs
 
 
   	//STARTING FROM AN ID > NUMBER
-	$recipients = User::where('id', '>', '108')->orderBy('id', 'asc')->take($mailing->recipients)->get();
+	$recipients = User::where('id', '>', '215')->orderBy('id', 'asc')->take($mailing->recipients)->get();
 	//   	  	foreach ($recipients as $recipient)
 	//   	  		dump($recipient->id);
 
